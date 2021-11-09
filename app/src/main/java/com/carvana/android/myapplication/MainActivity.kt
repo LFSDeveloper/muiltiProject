855f77f16560a008384e31f6f003e7e05ffa2390
Leonel Florin Selles <leonel.florinselles@carvana.com>
2021-11-09 13:50:07 -0600

    Adding Base Android Root Project.

---
+++ app/src/main/java/com/carvana/android/myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.carvana.android.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
